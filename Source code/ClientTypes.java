b267c89eec995f3ea414b3c3f3c8602cede0e30b
Ahmad Elshiltawi <85766582+TheNameIsAhmad@users.noreply.github.com>
2022-04-14 16:10:26 -0600

    added a toString abstract method

--- Source code/ClientTypes.java
+++ Source code/ClientTypes.java
@@ -1,22 +1,32 @@
 package edu.ucalgary.ensf409;
 
+/*
+ * ClientTypes is an enumeration that is used to have a consistent way of getting the ClientIDs of the different clients
+ */
+
 public enum ClientTypes {
     MALE {
         public int clientID() {
             return 1;
         }
+        public String toString() { return "Male"; }
     }, FEMALE {
         public int clientID() {
             return 2;
         }
+        public String toString() { return "Female"; }
     }, CHILDOE {
         public int clientID() {
             return 3;
         }
+        public String toString() { return "ChildOE"; }
     }, CHILDUE {
         public int clientID() {
             return 4;
         }
+        public String toString() { return "ChildUE"; }
+
     };
     public abstract int clientID();
+    public abstract String toString();
 }
