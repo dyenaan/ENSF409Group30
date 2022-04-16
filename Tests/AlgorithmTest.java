1fa3eae1703245df77bfa1b76e1569fc0358c3f4
Ahmad Elshiltawi <85766582+TheNameIsAhmad@users.noreply.github.com>
2022-04-16 16:36:00 -0600

    Added documentation

--- Tests/AlgorithmTest.java
+++ Tests/AlgorithmTest.java
@@ -5,9 +5,22 @@ import java.util.*;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
+/**
+ * @author Ahmad Khaled, Pansilu Wickramasinghe, Dyenaan Dapoet, Esohe Aideyan.
+ * @version 1.1
+ * @since 1.0
+ */
+
+/*
+ * The purpose of the AlgorithmTest class is to test the functionality of the Algorithm class.
+ */
+
 public class AlgorithmTest {
     Person[] emptyPeopleArray = new Person[0];
     ArrayList<String> usedItemIDs = new ArrayList<>();
+
+    /* The testInvalidAlgorithmConstructor method tests if the algorithm class throws an IllegalArgumentException
+    * if the constructor was given an invalid/empty person array. */
 
     @Test
     public void testInvalidAlgorithmConstructor() throws HamperAlreadyFoundException, StockNotAvailableException {
@@ -24,6 +37,9 @@ public class AlgorithmTest {
         assertTrue("Algorithm constructor did not throw an IllegalArgumentException when given an empty person array: ", correctException);
     }
 
+    /* The testAlgorithmGetFamilyCaloriesTotal method tests if the GetFamilyCaloriesTotal getter method returns
+    * the right value of familyTotalCalories or not. */
+
     @Test
     public void testAlgorithmGetFamilyCaloriesTotal() throws HamperAlreadyFoundException, StockNotAvailableException {
         Person[] people = new Person[1];
@@ -34,6 +50,9 @@ public class AlgorithmTest {
 
         assertEquals("The getFamilyNutritionCalories method did not return a value for Calories", totalCalories, expectedValue, 0.1);
     }
+
+    /* The testAlgorithmGetFamilyCaloriesWG method tests if the GetFamilyCaloriesTotal getter method returns
+     * the right value of familyWGCalories or not. */
 
     @Test
     public void testAlgorithmGetFamilyCaloriesWG() throws HamperAlreadyFoundException, StockNotAvailableException {
@@ -47,6 +66,9 @@ public class AlgorithmTest {
         assertEquals("The getFamilyCalories method did not return a value for whole grains", expectedValue, wholeGrains, 0.1);
     }
 
+    /* The testAlgorithmGetFamilyCaloriesWG method tests if the GetFamilyCaloriesTotal getter method returns
+     * the right value of familyProCalories or not. */
+
     @Test
     public void testAlgorithmGetFamilyCaloriesPro() throws HamperAlreadyFoundException, StockNotAvailableException {
         Person[] people = new Person[1];
@@ -58,6 +80,9 @@ public class AlgorithmTest {
 
         assertEquals("The getFamilyCalories method did not return a value for protein", expectedValue, protein, 0.1);
     }
+
+    /* The testAlgorithmGetFamilyCaloriesWG method tests if the GetFamilyCaloriesTotal getter method returns
+     * the right value of familyFVCalories or not. */
 
     @Test
     public void testAlgorithmGetFamilyCaloriesFV() throws HamperAlreadyFoundException, StockNotAvailableException {
@@ -71,6 +96,9 @@ public class AlgorithmTest {
         assertEquals("The getFamilyCalories method did not return a value for Fruit Veggies", expectedValue, fv, 0.1);
     }
 
+    /* The testAlgorithmGetFamilyCaloriesWG method tests if the GetFamilyCaloriesTotal getter method returns
+     * the right value of familyOtherCalories or not. */
+
     @Test
     public void testAlgorithmGetFamilyCaloriesOther() throws HamperAlreadyFoundException, StockNotAvailableException {
         Person[] people = new Person[1];
@@ -82,6 +110,9 @@ public class AlgorithmTest {
 
         assertEquals("The getFamilyCalories method did not return a value for other", expectedValue, other, 0.1);
     }
+
+    /* The testAlgorithmInvalidGetFamilyNutritionCalories method tests if the getter throws an IllegalArgumentException if
+    * given an invalid input or not. */
 
     @Test
     public void testAlgorithmInvalidGetFamilyNutritionCalories() throws HamperAlreadyFoundException, StockNotAvailableException {
@@ -100,6 +131,8 @@ public class AlgorithmTest {
         assertTrue("getFamilyNutritionCalories constructor did not throw an IllegalArgumentException when given an invalid argument: ", correctException);
     }
 
+    /* The testAlgorithmGetFBestHamper method tests if the getter returns the best hamper or not. */
+
     @Test
     public void testAlgorithmGetFBestHamper() throws HamperAlreadyFoundException, StockNotAvailableException {
         Person[] people = new Person[1];
@@ -108,6 +141,8 @@ public class AlgorithmTest {
         ArrayList<Map<String, String>> bestHamper = algorithm.getBestHamper();
         assertNotNull("The getBestHamper method did not return a hamper: ", bestHamper);
     }
+
+    /* The testAlgorithmGetUsedItemIDs method tests if the getter returns the usedItemIDs ArrayList or not. */
 
     @Test
     public void testAlgorithmGetUsedItemIDs() throws HamperAlreadyFoundException, StockNotAvailableException {
