2823ac29adee77de2feea1fddfabe77a5881f321
Ahmad Elshiltawi <85766582+TheNameIsAhmad@users.noreply.github.com>
2022-04-15 21:50:55 -0600

    Add files via upload

---
+++ Tests/Person.java
@@ -0,0 +1,49 @@
+package edu.ucalgary.ensf409;
+
+import java.util.Map;
+import java.util.Objects;
+
+public class Person {
+    private final int CLIENT_ID;
+    private final int CALORIES;
+    private final int WHOLE_GRAINS;
+    private final int FRUIT_VEGGIES;
+    private final int PROTEIN;
+    private final int OTHER;
+
+    Person(int clientID) {
+        if (!validateClientID(clientID)) throw new IllegalArgumentException();
+        Database db = new Database("jdbc:mysql://localhost/food_inventory", "student", "ensf");
+        db.initializeConnection();
+        Map<String, String> clientNeeds = db.selectClientNeeds(String.valueOf(clientID));
+        db.close();
+
+        this.CLIENT_ID = clientID;
+        this.CALORIES = Integer.parseInt(clientNeeds.get("Calories"));
+        this.WHOLE_GRAINS = Integer.parseInt(clientNeeds.get("WholeGrains"));
+        this.FRUIT_VEGGIES = Integer.parseInt(clientNeeds.get("FruitVeggies"));
+        this.PROTEIN = Integer.parseInt(clientNeeds.get("Protein"));
+        this.OTHER = Integer.parseInt(clientNeeds.get("Other"));
+    }
+
+    public double getNutrition(String type) {
+        if (Objects.equals(type, NutritionTypes.CALORIES.asString())) return this.CALORIES;
+        else if (Objects.equals(type, NutritionTypes.WHOLE_GRAINS.asString())) return this.WHOLE_GRAINS;
+        else if (Objects.equals(type, NutritionTypes.FRUIT_VEGGIES.asString())) return this.FRUIT_VEGGIES;
+        else if (Objects.equals(type, NutritionTypes.PROTEIN.asString())) return this.PROTEIN;
+        else if (Objects.equals(type, NutritionTypes.OTHER.asString())) return this.OTHER;
+        else throw new IllegalArgumentException("Did not recognize input!");
+    }
+
+    private boolean validateClientID(int clientID) {
+        if (clientID == ClientTypes.MALE.clientID()) return true;
+        else if (clientID == ClientTypes.FEMALE.clientID()) return true;
+        else if (clientID == ClientTypes.CHILDOE.clientID()) return true;
+        else if (clientID == ClientTypes.CHILDUE.clientID()) return true;
+        else return false;
+    }
+
+    public int getClientID() {
+        return this.CLIENT_ID;
+    }
+}
