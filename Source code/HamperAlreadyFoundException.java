b223cf7ea4dc22abf20660b7d6568f888e01aaa9
Ahmad Elshiltawi <85766582+TheNameIsAhmad@users.noreply.github.com>
2022-04-16 17:59:09 -0600

    added documentation

--- Source code/HamperAlreadyFoundException.java
+++ Source code/HamperAlreadyFoundException.java
@@ -1,5 +1,16 @@
 package edu.ucalgary.ensf409;
 
+/**
+ * @author Ahmad Khaled, Pansilu Wickramasinghe, Dyenaan Dapoet, Esohe Aideyan.
+ * @version 1.0
+ * @since 1.0
+ */
+
+/*
+ * HamperAlreadyFoundException is a custom exception that is thrown when the hamper is already found and the method
+ * findBestHamper is called.
+ */
+
 public class HamperAlreadyFoundException extends Exception{
     public HamperAlreadyFoundException(String e) {
         super(e);
--- Source code/StockNotAvailableException.java
+++ Source code/StockNotAvailableException.java
@@ -1,5 +1,15 @@
 package edu.ucalgary.ensf409;
 
+/**
+ * @author Ahmad Khaled, Pansilu Wickramasinghe, Dyenaan Dapoet, Esohe Aideyan.
+ * @version 1.0
+ * @since 1.0
+ */
+
+/*
+ * StockNotAvailableException when there isn't enough stock to get the best hamper
+ */
+
 public class StockNotAvailableException extends Exception{
     public StockNotAvailableException(String e) {
         super(e);
