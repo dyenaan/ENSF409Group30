99dfb05574c667225e7f5a5782ccc43236db23d2
Ahmad Elshiltawi <85766582+TheNameIsAhmad@users.noreply.github.com>
2022-04-16 18:03:44 -0600

    added documentation

--- Source code/Person.java
+++ Source code/Person.java
@@ -3,6 +3,17 @@ package edu.ucalgary.ensf409;
 import java.util.Map;
 import java.util.Objects;
 
+/**
+ * @author Ahmad Khaled, Pansilu Wickramasinghe, Dyenaan Dapoet, Esohe Aideyan.
+ * @version 1.3
+ * @since 1.0
+ */
+
+/*
+ * The purpose of the Person class is to save the data of a client of a specific client ID.
+ * All client data can be accessed via getters.
+ */
+
 public class Person {
     private final int CLIENT_ID;
     private final int CALORIES;
@@ -26,6 +37,8 @@ public class Person {
         this.OTHER = Integer.parseInt(clientNeeds.get("Other"));
     }
 
+    // Given a specific nutrition type, the getNutrition method returns the value of the nutrition for the client.
+
     public double getNutrition(String type) {
         if (Objects.equals(type, NutritionTypes.CALORIES.asString())) return this.CALORIES;
         else if (Objects.equals(type, NutritionTypes.WHOLE_GRAINS.asString())) return this.WHOLE_GRAINS;
@@ -35,6 +48,8 @@ public class Person {
         else throw new IllegalArgumentException("Did not recognize input!");
     }
 
+    // The validateClientID method returns true if the client ID exists within the ClientTypes enumeration. else returns false
+
     private boolean validateClientID(int clientID) {
         if (clientID == ClientTypes.MALE.clientID()) return true;
         else if (clientID == ClientTypes.FEMALE.clientID()) return true;
@@ -42,6 +57,8 @@ public class Person {
         else if (clientID == ClientTypes.CHILDUE.clientID()) return true;
         else return false;
     }
+
+    // The getClientID returns the client ID.
 
     public int getClientID() {
         return this.CLIENT_ID;
