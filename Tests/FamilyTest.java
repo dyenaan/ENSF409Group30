92af756c429914e202ccd3b251adef90033aaee7
Ahmad Elshiltawi <85766582+TheNameIsAhmad@users.noreply.github.com>
2022-04-17 11:15:56 -0600

    added documentation

--- Tests/FamilyTest.java
+++ Tests/FamilyTest.java
@@ -6,8 +6,18 @@ import org.junit.*;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
+/**
+ * @author Ahmad Khaled, Pansilu Wickramasinghe, Dyenaan Dapoet, Esohe Aideyan.
+ * @version 1.3
+ * @since 1.0
+ */
+
 /*
+ * The purpose of the FamilyTest class is to test the functionality of the Algorithm class.
+ *
  * Important note: There must be enough food items for at least one adult male for the tests to work properly!
+ * This is because the family class calls the Algorithm class to find the best hamper. If there isn't enough stock,
+ * then the test will error out (which is intended).
  */
 
 public class FamilyTest{
@@ -16,14 +26,14 @@ public class FamilyTest{
 	ArrayList<String> currentUsedItems = new ArrayList<String>();
 	
 	int mCount = 1;
-	int invMCount = -2;
+	int invMCount = -2; // Invalid male count
 	int fCount = 0;
-	int invFCount = -2;
 	int uECount = 0;
-	int invUECount = -1;
 	int oECount = 0;
-	int invOECount = -3;
-	
+
+
+	// The testFamilyConstructorInvalidInput tests the constructor class when given an invalid input.
+
 	@Test
 	public void testFamilyConstructorInvalidInput() throws HamperAlreadyFoundException, StockNotAvailableException{
 		
@@ -38,15 +48,18 @@ public class FamilyTest{
 		
 		assertTrue("Person constructor did not throw an IllegalArgumentException when given a count less than 0: ", correctException);
     }
-	
+
+	// The testFamilyConstructorValidInput tests the constructor class when given a valid input.
+
 	@Test
 	public void testFamilyConstructorValidInput() throws HamperAlreadyFoundException, StockNotAvailableException{
 		
         Family family = new Family(mCount,fCount,uECount,oECount, currentUsedItems);
         assertNotNull("Family constructor did not create an object when given a valid count:", family);
     }
-	
-	
+
+	// The testFamilyGetPeople method tests the getPeople getter
+
 	@Test
     public void testFamilyGetPeople() throws HamperAlreadyFoundException, StockNotAvailableException{
 		
@@ -55,7 +68,9 @@ public class FamilyTest{
 		
 		assertNotNull("Method getPeople did not return an object of type person: " , people);
     }
-	
+
+	// The testFamilyGetHamper method tests the getHamper getter
+
 	@Test
 	public void testFamilyGetHamper() throws HamperAlreadyFoundException, StockNotAvailableException{
 		Family nfamily = new Family(mCount,fCount,uECount,oECount, new ArrayList<String>());
@@ -64,6 +79,8 @@ public class FamilyTest{
 		ArrayList<Map<String, String>> bestHamper = nfamily.getHamper();
 		assertNotNull("Method getHamper did not return the best hamper: " , bestHamper);
 	}
+
+	// The testFamilyValidGetClientCount method tests the getClientCount getter if the input is valid
 
 	@Test
 	public void testFamilyValidGetClientCount() throws HamperAlreadyFoundException, StockNotAvailableException{
@@ -78,6 +95,8 @@ public class FamilyTest{
 		assertEquals("The childUE count value does not match the right value: ", childUECount, uECount);
 		assertEquals("The childOE count value does not match the right value: ", childOECount, oECount);
 	}
+
+	// The testFamilyValidGetClientCount method tests the getClientCount getter if the input is invalid
 
 	@Test
 	public void testFamilyInvalidGetClientCount() throws HamperAlreadyFoundException, StockNotAvailableException{
--- Tests/PersonTest.java
+++ Tests/PersonTest.java
@@ -5,19 +5,32 @@ import org.junit.*;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
+/**
+ * @author Ahmad Khaled, Pansilu Wickramasinghe, Dyenaan Dapoet, Esohe Aideyan.
+ * @version 1.3
+ * @since 1.0
+ */
+
+/*
+ *   The purpose of the PersonTest class is to test out the functionality of the Person class
+ */
+
 public class PersonTest{
 	
 	int validClientID = 1;
 	int negativeClientID = -2;
 	int invalidClientID = 5;
-	NutritionTypes nut;
-	
+
+    // The testPersonConstructorValidInput tests if the constructor works properly if given a valid input
+
 	@Test
     public void testPersonConstructorValidInput() {
         Person person = new Person(validClientID);
 		
         assertNotNull("Person constructor did not create an object when given a valid array of log entries.", person);
     }
+
+    // The testPersonConstructorNegativeInput tests if the constructor throws an IllegalArgumentException if given a negative input
 
     @Test
     public void testPersonConstructorNegativeInput() {
@@ -33,6 +46,8 @@ public class PersonTest{
         assertTrue("Person constructor did not throw an IllegalArgumentException when given a negative client ID: ", correctException);
     }
 
+    // The testPersonConstructorNegativeInput tests if the constructor throws an IllegalArgumentException if given a negative input
+
     @Test
     public void testPersonConstructorInvalidInput() {
         boolean correctException = false;
@@ -47,40 +62,25 @@ public class PersonTest{
         assertTrue("Person constructor did not throw an IllegalArgumentException when given an invalid client ID: ", correctException);
     }
 
-    @Test
-    public void testPersonGetCalories() {
-        Person person = new Person(validClientID);
-        int foundNumber = (int)person.getNutrition(nut.CALORIES);
-        assertNotNull("Method getNutrition did not return client calories value: ", foundNumber);
-    }
+    /* the testPersonInvalidGetNutrition tests if the getNutrition getter throws an IllegalArgumentException if an invalid
+    * input is given. */
 
     @Test
-    public void testPersonGetWholeGrains() {
+    public void testPersonInvalidGetNutrition() {
+        boolean correctException = false;
         Person person = new Person(validClientID);
-        int foundNumber = (int)person.getNutrition(nut.WHOLE_GRAINS);
-        assertNotNull("Method getNutrition did not return client whole grain value: ", foundNumber);
+
+        try{
+            double nutrition = person.getNutrition("Invalid");
+        }
+        catch(IllegalArgumentException e){
+            correctException = true;
+        }
+
+        assertTrue("Person constructor did not throw an IllegalArgumentException when given an invalid client ID: ", correctException);
     }
 
-    @Test
-    public void testPersonGetProtein() {
-        Person person = new Person(validClientID);
-        int foundNumber = (int)person.getNutrition(nut.PROTEIN);
-        assertNotNull("Method getNutrition did not return client protein value: ", foundNumber);
-    }
-
-    @Test
-    public void testPersonGetFruitVeggies() {
-        Person person = new Person(validClientID);
-        int foundNumber = (int)person.getNutrition(nut.FRUIT_VEGGIES);
-        assertNotNull("Method getNutrition did not return client fruit veggies value: ", foundNumber);
-    }
-
-    @Test
-    public void testPersonGetOther() {
-        Person person = new Person(validClientID);
-        int foundNumber = (int)person.getNutrition(nut.OTHER);
-        assertNotNull("Method getNutrition did not return other value: ", foundNumber);
-    }
+    // the testPersonGetClientID tests if the getClientID getter returns the client ID
 
     @Test
     public void testPersonGetClientID() {
@@ -89,7 +89,5 @@ public class PersonTest{
         int expectedClientID = validClientID;
         assertEquals("Method getClientID did not return the expected result: ", expectedClientID, foundClientID);
     }
-	
-	
-	
+
 }
